e36b218cc15e80e94c57a5d6bc9472202e356f84
Isaiah <mancha.isaiah@gmail.com>
2024-08-17 10:33:38 -0400

    dom mod

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const mainNode = document.querySelector('main#main');
+if (mainNode) {
+  mainNode.remove();
+}
+const newHeader = document.createElement('h1');
+newHeader.textContent = 'New Header Text';
+newHeader.id = 'victory'; // Set the id of the h1 element to 'victory'
+document.body.appendChild(newHeader);
+newHeader.innerHTML = "Isaiah is the champion";
