5af82d8c8f24bdd8da2f61bd3f6d79789f2fcfea
Isaiah <mancha.isaiah@gmail.com>
2024-08-28 14:27:39 -0400

    lab

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const mainNode = document.querySelector('main#main');
-if (mainNode) {
-  mainNode.remove();
-}
-const newHeader = document.createElement('h1');
-newHeader.textContent = 'New Header Text';
-newHeader.id = 'victory'; // Set the id of the h1 element to 'victory'
-document.body.appendChild(newHeader);
-newHeader.innerHTML = "Isaiah is the champion";
+const mainElement = document.getElementById('main')
+  mainElement.remove()
+
+const newHeader = document.createElement('h1')
+
+newHeader.id = 'victory'
+
+newHeader.textContent = "Isaiah is the champion"
